3375a7a1f3c0e7591bd25876b591d7130a9aeb90
moez-rd <slow9ie@gmail.com>
2022-08-03 20:32:49 +0700

    Update

--- components/OpeningHourItem/OpeningHourItem.tsx
+++ components/OpeningHourItem/OpeningHourItem.tsx
@@ -8,9 +8,9 @@ interface Props {
 const OpeningHourItem: FC<Props> = ({ day, hour }) => {
   return (
     <li className="flex items-baseline justify-between py-2 lg:flex-col xl:flex-row">
-      <p className="text-xl font-medium">{day}</p>
-      <div className="mx-2 flex-grow border-b border-b-primary-700 lg:hidden xl:block"></div>
-      <p>{hour}</p>
+      <p className="text-xl font-bold text-primary-50">{day}</p>
+      <div className="mx-2 flex-grow border-b border-b-primary-300 lg:hidden xl:block"></div>
+      <p className="text-primary-300">{hour}</p>
     </li>
   );
 };
